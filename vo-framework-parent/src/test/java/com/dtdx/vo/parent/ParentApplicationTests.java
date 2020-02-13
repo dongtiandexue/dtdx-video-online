8a872bed89ff0d39f452b1339b763257ce421c50
dongtiandexue <huawei_code@163.com>
2020-02-13 14:57:09 +0800

    Initial commit

---
+++ vo-framework-parent/src/test/java/com/dtdx/vo/parent/ParentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dtdx.vo.parent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ vo-framework-utils/src/test/java/com/dtdx/vo/voframeworkutils/VoFrameworkUtilsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dtdx.vo.voframeworkutils;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VoFrameworkUtilsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
